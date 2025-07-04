10ae633315783a9f2397444a31b0d3cbcf819ef2
Alireza Nobakht <nobakht.job@gmail.com>
2025-07-04 09:31:26 +0200

    Initial commit: Rails API setup, route, controller, dry-container

---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,9 @@
+
+class GuessesController < ApplicationController
+  def show
+    # This action will render the view for the country guess game.
+    # You can add any logic here if needed, such as initializing variables.
+
+    # For example, you might want to set a random country for the user to guess:
+  end
+end
